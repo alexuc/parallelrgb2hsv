9f43543aee52aac191b06ff12ccf346d0d56e64f
alexuc <alexuc@gmail.com>
2015-11-19 19:32:47 -0600

    Update convert.cpp

--- convert.cpp
+++ convert.cpp
@@ -8,21 +8,22 @@ using namespace std;
 using namespace cv;
 
 
-
+// Routine to convert a RGB pixel to HSV. This function is based on fixed point data types and short-circuit comparisons
+// to improve performance
 static void RGB2HSV(unsigned char r, unsigned char g, unsigned char b, unsigned char &h, unsigned char &s, unsigned char &v)
 {
     unsigned char minRGB, maxRGB;
 
     minRGB = r;
     maxRGB = r;
-    //minRGB = r < g ? (r < b ? r : b) : (g < b ? g : b);
-    //maxRGB = r > g ? (r > b ? r : b) : (g > b ? g : b);
-    (minRGB > g) && (minRGB = g); //these are not conditional statements.
-    (minRGB > b) && (minRGB = b); //these are just boolean expressions.
-    (maxRGB < g) && (maxRGB = g); //these are not conditional statements.
-    (maxRGB < b) && (maxRGB = b); //these are just boolean expressions.
+    (minRGB > g) && (minRGB = g); //these are short-circuit comparisons
+    (minRGB > b) && (minRGB = b); //these are short-circuit comparisons
+    (maxRGB < g) && (maxRGB = g); //these are short-circuit comparisons
+    (maxRGB < b) && (maxRGB = b); //these are short-circuit comparisons
 
     v = maxRGB;
+	
+	// if value is 0, no need to calculate anything else
     if (v == 0)
     {
         h = 0;
@@ -30,6 +31,8 @@ static void RGB2HSV(unsigned char r, unsigned char g, unsigned char b, unsigned 
         return;
     }
 
+	// find the S value, according to native formula
+	// we know v is not zero
     s = 255 * (maxRGB - minRGB) / v;
     if (s == 0)
     {
@@ -37,31 +40,70 @@ static void RGB2HSV(unsigned char r, unsigned char g, unsigned char b, unsigned 
         return;
     }
 
+	// find the H value, according to native formula
     if (r == maxRGB)
         h = 0 + 43 * (g - b) / (maxRGB - minRGB);
     else if (g == maxRGB)
         h = 85 + 43 * (b - r) / (maxRGB - minRGB);
     else
         h = 171 + 43 * (r - g) / (maxRGB - minRGB);
+}
 
-    return;
+// Routine to convert a RGB pixel to HSV. This function lacks the short-circuit comparison optimization
+static void RGB2HSVbasic(unsigned char r, unsigned char g, unsigned char b, unsigned char &h, unsigned char &s, unsigned char &v)
+{
+    unsigned char minRGB, maxRGB;
+
+    minRGB = r;
+    maxRGB = r;
+	// comparisons using condition-based statements
+    minRGB = r < g ? (r < b ? r : b) : (g < b ? g : b);
+    maxRGB = r > g ? (r > b ? r : b) : (g > b ? g : b);
+
+    v = maxRGB;
+	
+	// if value is 0, no need to calculate anything else
+    if (v == 0)
+    {
+        h = 0;
+        s = 0;
+        return;
+    }
+
+	// find the S value, according to native formula
+	// we know v is not zero
+    s = 255 * (maxRGB - minRGB) / v;
+    if (s == 0)
+    {
+        h = 0;
+        return;
+    }
+
+	// find the H value, according to native formula
+    if (r == maxRGB)
+        h = 0 + 43 * (g - b) / (maxRGB - minRGB);
+    else if (g == maxRGB)
+        h = 85 + 43 * (b - r) / (maxRGB - minRGB);
+    else
+        h = 171 + 43 * (r - g) / (maxRGB - minRGB);
 }
 
 
+// Routine to convert a RGB pixel to HSV using float types. This function is the most straight form of implementation
+// as it lacks all optimizations, however it keeps the deepest precision by using float types
 static void RGB2HSVf(float r, float g, float b, float &h, float &s, float &v)
 {
     float minRGB, maxRGB;
 
     minRGB = r;
     maxRGB = r;
-    //minRGB = r < g ? (r < b ? r : b) : (g < b ? g : b);
-    //maxRGB = r > g ? (r > b ? r : b) : (g > b ? g : b);
-    (minRGB > g) && (minRGB = g); //these are not conditional statements.
-    (minRGB > b) && (minRGB = b); //these are just boolean expressions.
-    (maxRGB < g) && (maxRGB = g); //these are not conditional statements.
-    (maxRGB < b) && (maxRGB = b); //these are just boolean expressions.
+	// comparisons using condition-based statements
+    minRGB = r < g ? (r < b ? r : b) : (g < b ? g : b);
+    maxRGB = r > g ? (r > b ? r : b) : (g > b ? g : b);
 
     v = maxRGB;
+	
+	// if value is 0, no need to calculate anything else
     if (v == 0.f)
     {
         h = 0.f;
@@ -69,6 +111,8 @@ static void RGB2HSVf(float r, float g, float b, float &h, float &s, float &v)
         return;
     }
 
+	// find the S value, according to native formula
+	// we know v is not zero
     s = 255.f * (maxRGB - minRGB) / v;
     if (s == 0.f)
     {
@@ -76,19 +120,19 @@ static void RGB2HSVf(float r, float g, float b, float &h, float &s, float &v)
         return;
     }
 
+	// find the H value, according to native formula
     if (r == maxRGB)
         h = 43.f * (g - b) / (maxRGB - minRGB);
     else if (g == maxRGB)
         h = 128.f * (b - r) / (maxRGB - minRGB);
     else
         h = 214.f * (r - g) / (maxRGB - minRGB);
-
-    return;
 }
 
 
-static void RGB2HSV2(float r, float g, float b,
-                    float &h, float &s, float &v)
+// Routine to convert a RGB pixel to HSV using float types. This function is optimized using floating point spaces
+// code taken from: http://lolengine.net/blog/2013/01/13/fast-rgb-to-hsv
+static void RGB2HSV2(float r, float g, float b, float &h, float &s, float &v)
 {
     float K = 0.f;
 
@@ -115,73 +159,105 @@ static void RGB2HSV2(float r, float g, float b,
 int main(int argc, char** argv )
 {
     double elapsedTime;
-    const char* inputFileName = "soccer.avi";
-    const char* outputFileName = "soccer2.avi";
+	// declare input and output file names, according to user input
+    const char* inputFileName = argv[0];
+    const char* outputFileName = argv[1];
 
     // starting timer
     timerStart();
 
+	// declare OpenCV's video capture
     VideoCapture cap(inputFileName);
 
+	// detect codec ID from video file
     int ex = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));
 
     printf("CodecID: %i\n", ex);
 
+	// declare OpenCV's video writer object
     VideoWriter out;
-    Size size = Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH),    // Acquire input size
-      (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));
+	
+	// Acquire input size from video metadata
+    Size size = Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH), (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));
 
-    if(!cap.isOpened())  // check if we succeeded
+	// check if video was successfully opened
+    if(!cap.isOpened())
         return -1;
 
     int cols = size.width;
     int rows = size.height;
 
-    printf("Video size: %i x %i\n", rows, cols);
+    printf("Size %i - %i\n", rows, cols);
 
+	// open the video writer onto the output file, using the codec ID identified
     out.open(outputFileName, ex, cap.get(CV_CAP_PROP_FPS), size, true);
 
+	// check if output file was opened
     if (!out.isOpened())
     {
-        cout  << "Could not open the output video for write: " << endl;
+        printf("Could not open the output video for write.\n");
         return -1;
     }
 
+	// read number of frames in video file
     long count = (long)cap.get(CV_CAP_PROP_FRAME_COUNT);
 
     printf("Frames %d\n", (int)count);
 
-    Mat edges;
-    Mat frame;
-    Vec3b color;
+	// declare matrix source and target
+    Mat targetFrame;
+    Mat sourceFrame;
 
+	// main loop to go through each of the frames in the video
     for(int counter = 0; counter < count; counter++)
-    //for(;;)
     {
-        cap >> frame;
+		// acquire frame from input video capture
+        cap >> sourceFrame;
 
-        if (frame.empty()) break;
-        frame.copyTo(edges);
+		// prevent code from reading an empty frame
+        if (sourceFrame.empty()) break;
+		
+		// create a copy of the frame just read
+        sourceFrame.copyTo(targetFrame);
 
-        int channels = edges.channels();
+		// obtain the number of channels, this is only needed to access the right channel
+		// when fetching the frame, as the file could have other channels than audio and video
+		// this is only needed to make the code agnostic to multi-channel videos
+        int channels = targetFrame.channels();
 
+		// loop to go through each pixel in the frame. parallel area initiates
+		// 2nd level loop, access matrix rows
         #pragma omp parallel for
         for(int j = 0;j < rows;j++) {
+			// 3rd level loop, access matrix columns
             for(int i = 0;i < cols;i++) {
+				// calculate the index of the pixel in the frame
                 int index = channels * (cols * j + i);
-                unsigned char b = edges.data[index], g = edges.data[index + 1], r = edges.data[index + 2], h,s,v;
-                //float b = edges.data[index], g = edges.data[index + 1], r = edges.data[index + 2], h,s,v;
+				
+				// obtain RGB values from video
+                unsigned char b = targetFrame.data[index], g = targetFrame.data[index + 1], r = targetFrame.data[index + 2]
+				// declare target HSV variables
+				unsigned char h,s,v;
+				
+				// code commented, needed only if we want the program use floating space mode
+                //float b = targetFrame.data[index], g = targetFrame.data[index + 1], r = targetFrame.data[index + 2], h,s,v;
                 //RGB2HSVf(r, g, b, h, s, v);
+				
+				// perform the actual conversion of the pixel
                 RGB2HSV(r, g, b, h, s, v);
-                edges.data[index] = h;
-                edges.data[index + 1] = s;
-                edges.data[index + 2] = v;
+				
+				// store converted pixel into target frame
+                targetFrame.data[index] = h;
+                targetFrame.data[index + 1] = s;
+                targetFrame.data[index + 2] = v;
             }
         }
 
-        out.write(edges);
+		// write the frame already converted to HSV into the output file
+        out.write(targetFrame);
     }
 
+	// close all captures opened in OpenCV
     destroyAllWindows();
 
     // stopping timer
